5cb84d35cb50580743629b68335a47fce3726938
RaquelCC <rcanalesconcha@gmail.com>
2019-06-20 01:06:14 -0400

    Updates

---
+++ precache-manifest.970ad3309b8bdcec41e8c725cabde089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885bf8717835ad7b9d7e0b3c415977d9",
+    "url": "/desafio-cumplo/index.html"
+  },
+  {
+    "revision": "c0e66086d48142027131",
+    "url": "/desafio-cumplo/static/css/2.6b197f1e.chunk.css"
+  },
+  {
+    "revision": "d5bec11abcf8dd8d2780",
+    "url": "/desafio-cumplo/static/css/main.bc006299.chunk.css"
+  },
+  {
+    "revision": "c0e66086d48142027131",
+    "url": "/desafio-cumplo/static/js/2.2d85dddf.chunk.js"
+  },
+  {
+    "revision": "d5bec11abcf8dd8d2780",
+    "url": "/desafio-cumplo/static/js/main.bc2dcbf8.chunk.js"
+  },
+  {
+    "revision": "1a1ff487ff0cf9bfd100",
+    "url": "/desafio-cumplo/static/js/runtime~main.d640573f.js"
+  },
+  {
+    "revision": "bd82bdbe49bd0bc65e26932fe58a3701",
+    "url": "/desafio-cumplo/static/media/sharon-mccutcheon-665638-unsplash.bd82bdbe.jpg"
+  }
+]);
